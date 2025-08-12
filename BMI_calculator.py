cbd43c1a123c473e6a061fca360b04576269906c
Dhilipkumar-2002 <dhilipkumarkv@gmail.com>
2025-08-12 10:36:07 +0530

    BMI_calculator.py

--- BMI_calculator.py
+++ BMI_calculator.py
@@ -10,15 +10,16 @@ if st.button("Calculate BMI"):
     bmi = w / ((h / 100) ** 2)
     st.write(f"**Your BMI:** {bmi:.2f}")
     if bmi < 18.5:
-        st.warning("You are Underweight, nalla sapudanum")
+        st.warning("You are Underweight, you must hit the gym and take suffucient intake of protein, fibre and carbohydrate.")
     elif 18.5 <= bmi <= 24.9:
-        st.success("Normal weight, nee polachiduva da")
+        st.success("You are Normal weight, You are at your optimal weight, check for fat percentage in your body")
     elif 25 <= bmi <= 29.9:
-        st.info("Overweight - gundu punda")
+        st.info("You are Overweight, Regular Excercise and consistenely following proper diet wouild improve you health, Buddy")
     elif 30 <= bmi <= 39.9:
-        st.error("Obese - kolutha punda")
+        st.error("You are Obese, Consult a doctor and take care of your health")
     else:
-        st.error("Extremely Obese - ithu enna vaira illa paanaya")
+        st.error("You are Extremely Obese, must admit to hospital --> immediately")
+
 
 
 
